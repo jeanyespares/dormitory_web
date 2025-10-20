4c9a926ac48a02cca633933a821354e40be6e151
jeanyespares <jeanyespares404@gmail.com>
2025-10-20 08:33:19 +0800

    try

--- app/config/routes.php
+++ app/config/routes.php
@@ -9,4 +9,3 @@ $router->post('/tenants/store', 'Tenants::store');
 $router->get('/tenants/edit/(:num)', 'Tenants::edit/$1');
 $router->post('/tenants/update/(:num)', 'Tenants::update/$1');
 $router->get('/tenants/delete/(:num)', 'Tenants::delete/$1');
-g
