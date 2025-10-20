cc04e34a38a95090eb8a7a9b3f165e78c214cc7a
jeanyespares <jeanyespares404@gmail.com>
2025-10-20 12:55:04 +0800

    try

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -1,53 +1,43 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-/**
- * ------------------------------------------------------------------
- * LavaLust - an opensource lightweight PHP MVC Framework
- * ------------------------------------------------------------------
- * AUTO-LOADER CONFIGURATION FILE
- * ------------------------------------------------------------------
- * This file specifies which systems should be loaded by default.
- * ------------------------------------------------------------------
- */
+/*
+|--------------------------------------------------------------------------
+| AUTO-LOADER CONFIGURATION
+|--------------------------------------------------------------------------
+| Specifies which systems should be loaded by default.
+|--------------------------------------------------------------------------
+*/
 
 /*
-| -------------------------------------------------------------------
-|  Auto-load Libraries
-| -------------------------------------------------------------------
-| Example:
-|   $autoload['libraries'] = array('database', 'session', 'email');
+|--------------------------------------------------------------------------
+| Auto-load Libraries
+|--------------------------------------------------------------------------
+| Load essential libraries automatically
 */
 $autoload['libraries'] = array('database');
 
 /*
-| -------------------------------------------------------------------
-|  Auto-load Helper Files
-| -------------------------------------------------------------------
-| Example:
-|   $autoload['helpers'] = array('url', 'form');
+|--------------------------------------------------------------------------
+| Auto-load Helper Files
+|--------------------------------------------------------------------------
+| Common helper files used in the app
 */
 $autoload['helpers'] = array('url', 'form');
 
 /*
-| -------------------------------------------------------------------
-|  Auto-load Models
-| -------------------------------------------------------------------
-| Example:
-|   $autoload['models'] = array('User_model', 'Tenant_model');
-|
-| Add all models you want to be available globally.
+|--------------------------------------------------------------------------
+| Auto-load Models
+|--------------------------------------------------------------------------
+| Models that will always be available
 */
 $autoload['models'] = array('Tenant_model');
 
 /*
-| -------------------------------------------------------------------
-|  Auto-load Config Files
-| -------------------------------------------------------------------
-| Example:
-|   $autoload['configs'] = array('custom_config');
-|
-| Only use this if you have your own custom config files.
+|--------------------------------------------------------------------------
+| Auto-load Config Files
+|--------------------------------------------------------------------------
+| Add custom config files if needed
 */
 $autoload['configs'] = array();
 ?>
